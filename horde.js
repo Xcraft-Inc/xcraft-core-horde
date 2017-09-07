2b33ad10cc9d1075779bc79378f708bc0bbffeb5
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-09-07 16:18:10 +0200

    Add a first draft for an Xcraft module for daemonizing Xcraft servers
    The final goal will be to manage a pool (the Horde) of servers to scale
    the goblins. For example, it will be possible to have the warehouse on
    its own process.

---
+++ horde.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const cmd = {};
+
+cmd['slave.add'] = function (msg, resp) {
+  const horde = require ('.');
+
+  try {
+    horde.add (resp);
+    resp.events.send (`horde.slave.add.${msg.id}.finished`);
+  } catch (ex) {
+    resp.events.send (`horde.slave.add.${msg.id}.error`);
+  }
+};
+
+cmd['slave.remove'] = function (msg, resp) {
+  const horde = require ('.');
+
+  try {
+    horde.remove (msg.data.pid, resp);
+    resp.events.send (`horde.slave.remove.${msg.id}.finished`);
+  } catch (ex) {
+    resp.events.send (`horde.slave.remove.${msg.id}.error`);
+  }
+};
+
+/**
+ * Retrieve the list of available commands.
+ *
+ * @returns {Object} The list and definitions of commands.
+ */
+exports.xcraftCommands = function () {
+  return {
+    handlers: cmd,
+    rc: {
+      'slave.add': {
+        parallel: true,
+        desc: 'add a slave in the Horde',
+        options: {
+          params: {
+            required: 'pid',
+          },
+        },
+      },
+      'slave.remove': {
+        parallel: true,
+        desc: 'remove a slave from the Horde',
+      },
+    },
+  };
+};
