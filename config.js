6d026de15b3d6ee84b6003c94b8707f7fae255c9
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-01-29 15:30:27 +0100

    WIP: add config file for some modules
    Not used...

---
+++ config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Retrieve the inquirer definition for xcraft-core-etc
+ */
+module.exports = [
+  {
+    type: 'checkbox',
+    name: 'hordes',
+    message: 'autoloaded hordes',
+    default: [],
+  },
+];
